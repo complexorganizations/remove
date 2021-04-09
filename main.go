31ebe4997008e71e5a19c4fef00c21668ac3520d
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-04-09 15:21:22 -0400

    Update main.go

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"os"
+	"log"
 )
 
 var filePath string
@@ -19,6 +20,8 @@ func main() {
 		os.Remove(filePath)
 	} else if folderExists(filePath) {
 		os.RemoveAll(filePath)
+	} else {
+		log.Fatal("Error: No such file or directory")
 	}
 }
 
