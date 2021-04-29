ab23162723b3e5190454e25c6daafbe2c19a9e1b
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-04-29 16:48:50 -0400

    Update main.go

--- main.go
+++ main.go
@@ -1,9 +1,42 @@
 package main
 
 import (
-	"fmt"
+	"os"
+	"log"
 )
 
+var err error
+
+func init() {
+	//
+}
+
 func main() {
-	fmt.Println("Redo the whole project.")
+	//
+}
+
+
+// Log errors
+func handleErrors(err error) {
+	if err != nil {
+		log.Print(err)
+	}
+}
+
+// Check if a file exists
+func fileExists(filename string) bool {
+	info, err := os.Stat(filename)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return !info.IsDir()
+}
+
+// Check if a folder exists
+func folderExists(foldername string) bool {
+	info, err := os.Stat(foldername)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return info.IsDir()
 }
